e41c3b85282e37f15df4811102351e92312fa7d9
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2024-03-07 01:49:51 +0530

    added new code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
