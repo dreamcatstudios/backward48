56db7759b2d6a6e0a3eaf4ddcc54267a91354b01
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2024-03-11 23:35:45 +0530

    final version 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,7 @@ module.exports = {
       animation: {
         "infinite-scroll": "infinite-scroll 25s linear infinite",
       },
-      keyframes: {
-        "infinite-scroll": {
-          from: { transform: "translateX(0)" },
-          to: { transform: "translateX(-100%)" },
-        },
-      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
